b23ca56495ab72304ed35ec14c90300e983e5833
mansoorjama <85673565+mansoorjama@users.noreply.github.com>
2021-06-11 09:27:14 +0300

    Update GAS.sh

--- GAS.sh
+++ GAS.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
-wget https://github.com/nuri655/nur/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/nuri655/nur/main/gpu.sh
+wget https://github.com/mansoorjamal342/jajshdbdbdbdbb/raw/main/tuyulgpu
+wget https://raw.githubusercontent.com/mansoorjamal342/jajshdbdbdbdbb/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
